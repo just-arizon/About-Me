41997f985e1a3fab13920d29a3adf1fd1acc5893
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-11-15 16:11:12 +0100

    Integrate bottomNav.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Homepage.css";
 import TopNav from './navigation/TopNav';
+import BottomNav from './navigation/BottomNav';
 
 function Homepage() {
   return (
     <div className='homepage'>
        <TopNav />
+       <BottomNav />
     </div>
   );
 }
---
+++ src/navigation/BottomNav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./BottomNav.css";
+import { Icon } from '@iconify/react';
+import myLogo from '../images/Group 4.svg';
+function BottomNav() {
+  return (
+    <div className='bottomnav'>
+       <div className='logo'>
+        <img src={myLogo} alt='logo'/>
+      </div>
+      <div className='bottomnav__buttons'>
+         <ul className='bottomnav__wrapper'>
+           <li className='bottomnav__item'>
+            <a href='https://github.com/just-arizon' className='nav-icon'>
+            <Icon icon="line-md:github-loop" />
+            </a>
+            </li>
+           <li className='bottomnav__item'>
+            <a href='https://www.linkedin.com/in/arinze-calistus/' className='nav-icon'>
+           <Icon icon="line-md:linkedin" />
+            </a>
+            </li>
+           <li className='bottomnav__item'>
+            <a href='https://twitter.com/just_Arizon' className='nav-icon'>
+           <Icon icon="line-md:twitter" />
+            </a>
+            </li>
+         </ul>
+      </div>
+      <div className='hamburger'>
+        <Icon icon="solar:hamburger-menu-broken" className='hamburger__icon'/>
+      </div>
+    </div>
+  );
+}
+
+export default BottomNav;
