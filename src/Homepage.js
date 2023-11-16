e84d2acceadf7e654e4801a97c32da0d5624da2a
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-11-16 11:44:19 +0100

    Add dashboard elements

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import "./Homepage.css";
 import TopNav from './navigation/TopNav';
 import BottomNav from './navigation/BottomNav';
+import Dashboard from './Dashboard';
 
 function Homepage() {
   return (
     <div className='homepage'>
        <TopNav />
+       <Dashboard />
        <BottomNav />
     </div>
   );
--- src/dashboard.js
+++ src/dashboard.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./Dashboard.css";
 
 function dashboard() {
   return (
-    <div>
-      
+    <div className="dashboard__component">
+      <div className="dash__timeline">
+        <div className="dash__profile--img">
+          <div class="image__circle">
+            <img src="" />
+            </div>
+        </div>
+        <div className="dash__profile">
+          <div className="dash__profile--text">
+            <p><span>Hello</span>, I’m Arinze 
+ A Front-End Developer with the following stacks
+</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
