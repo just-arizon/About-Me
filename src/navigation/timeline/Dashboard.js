94be526f3581c207e8e9d82a6cf183417f215425
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-11-16 12:21:43 +0100

    Add stacks

--- src/navigation/timeline/Dashboard.js
+++ src/navigation/timeline/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Dashboard.css";
-
+import { Icon } from '@iconify/react';
 function dashboard() {
   return (
     <div className="dashboard__component">
@@ -13,8 +13,32 @@ function dashboard() {
         <div className="dash__profile">
           <div className="dash__profile--text">
             <p><span>Hello</span>, I’m Arinze 
+            <br/>
  A Front-End Developer with the following stacks
 </p>
+<div className="stacks">
+  <span className="iconify toolkits">
+  <Icon icon="material-symbols:javascript-rounded" />
+  </span>
+  <span className="iconify toolkits">
+  <Icon icon="ion:logo-sass" />
+  </span>
+  <span className="iconify toolkits">
+  <Icon icon="tabler:brand-bootstrap" />
+  </span>
+  <span className="iconify toolkits">
+  <Icon icon="cib:react" />
+  </span>
+  {/* <span className="iconify toolkits">
+  <Icon icon="cib:react" />
+  </span>
+  <span className="iconify toolkits">
+  <Icon icon="cib:react" />
+  </span>
+  <span className="iconify toolkits">
+  <Icon icon="cib:react" />
+  </span> */}
+</div>
           </div>
         </div>
       </div>
