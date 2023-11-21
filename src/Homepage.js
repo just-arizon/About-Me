dd6f8c8ecd759253942704fad996d0006fb8a561
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-11-21 20:30:37 +0100

    Add hamburger menu

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import "./Homepage.css";
 import TopNav from './navigation/TopNav';
+
 import BottomNav from './navigation/BottomNav';
 import Dashboard from './timeline/Dashboard';
 import Scroller from './timeline/Swiper';
-
-
 function Homepage() {
   return (
     <div className='homepage'>
--- src/navigation/BottomNav.js
+++ src/navigation/BottomNav.js
@@ -1,36 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./BottomNav.css";
 import { Icon } from '@iconify/react';
 import myLogo from '../images/Group 4.svg';
 import { useTour } from '@reactour/tour'
 function BottomNav() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className='bottomnav'>
-       <div className='logo third-step'>
-        <img src={myLogo} alt='logo'/>
+
+     <div className={`submenu ${isMenuOpen ? 'open' : ''}`}>
+      <ul className='submenu__content'>
+             <li className='submenu__item'>
+              <a href='https://github.com/just-arizon' className='submenu__links'>
+              Project
+              </a>
+              </li>
+             <li className='submenu__item'>
+              <a href='https://www.linkedin.com/in/arinze-calistus/' className='submenu__links'>
+             About
+              </a>
+              </li>
+             <li className='submenu__item'>
+              <a href='https://twitter.com/just_Arizon' className='submenu__links'>
+             Experience
+              </a>
+              </li>
+             <li className='submenu__item'>
+              <a href='https://twitter.com/just_Arizon' className='submenu__links'>
+             Certificate
+              </a>
+              </li>
+           </ul>
       </div>
-      <div className='bottomnav__buttons'>
-         <ul className='bottomnav__wrapper fourth-step'>
-           <li className='bottomnav__item'>
-            <a href='https://github.com/just-arizon' className='nav-icon'>
-            <Icon icon="line-md:github-loop" />
-            </a>
-            </li>
-           <li className='bottomnav__item'>
-            <a href='https://www.linkedin.com/in/arinze-calistus/' className='nav-icon'>
-           <Icon icon="line-md:linkedin" />
-            </a>
-            </li>
-           <li className='bottomnav__item'>
-            <a href='https://twitter.com/just_Arizon' className='nav-icon'>
-           <Icon icon="line-md:twitter" />
-            </a>
-            </li>
-         </ul>
-      </div>
-      <div className='hamburger fifth-step'>
-        <Icon icon="solar:hamburger-menu-broken" className='hamburger__icon'/>
-      </div>
+    
+       <div class="bottomNav__container">
+         <div className='logo third-step'>
+          <img src={myLogo} alt='logo'/>
+               </div>
+               <div className='bottomnav__buttons'>
+           <ul className='bottomnav__wrapper fourth-step'>
+             <li className='bottomnav__item'>
+              <a href='https://github.com/just-arizon' className='nav-icon'>
+              <Icon icon="line-md:github-loop" />
+              </a>
+              </li>
+             <li className='bottomnav__item'>
+              <a href='https://www.linkedin.com/in/arinze-calistus/' className='nav-icon'>
+             <Icon icon="line-md:linkedin" />
+              </a>
+              </li>
+             <li className='bottomnav__item'>
+              <a href='https://twitter.com/just_Arizon' className='nav-icon'>
+             <Icon icon="line-md:twitter" />
+              </a>
+              </li>
+           </ul>
+               </div>
+               <div className='hamburger fifth-step'>
+          <Icon icon="solar:hamburger-menu-broken" className={`hamburger__icon ${isMenuOpen ? '' : ''}`} onClick={toggleMenu}/>
+               </div>
+       </div>
     </div>
   );
 }
