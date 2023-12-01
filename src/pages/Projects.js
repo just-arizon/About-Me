093ae4937f4c25889cf06311d79e578b38625b40
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-12-01 11:49:35 +0100

    Design Project Section first phase

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import "./Projects.css";
+import TopNav from "../navigation/TopNav";
 
 function Projects() {
   return (
-    <h1>
-      Hello World !!
-    </h1>
+    <div className="project__container">
+        <TopNav />
+      <h1>
+       Projects
+      </h1>
+
+      <div class="card">
+  <div class="tools">
+    <div class="circle">
+      <span class="red box"></span>
+    </div>
+    <div class="circle">
+      <span class="yellow box"></span>
+    </div>
+    <div class="circle">
+      <span class="green box"></span>
+    </div>
+  </div>
+  <div class="card__content">
+  </div>
+</div>
+    </div>
+
+    
   );
 }
 
