d1014a89d6ff59e434e06db879f26244155f8282
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-12-07 01:17:07 +0100

    Add cards to project section

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Projects.css";
 import TopNav from "../navigation/TopNav";
 
 function Projects() {
+ // Replace this with the actual number of cards
+ const numberOfCards = 3; // For example, if you have 3 cards
+
+ const initialCardStates = Array.from({ length: numberOfCards }, () => '');
+
+ const [cardStates, setCardStates] = useState(initialCardStates);
+
+ const handleSwitch = (className, index) => {
+   const newCardStates = [...cardStates];
+
+   switch (className) {
+     case "yellow":
+       newCardStates[index] = 'minimized';
+       break;
+     case "green":
+       newCardStates[index] = '';
+       break;
+     case "red":
+       newCardStates[index] = 'none';
+       break;
+     default:
+       // Handle default case
+   }
+
+   setCardStates(newCardStates);
+  };
+
   return (
-    <div className="project__container">
-      <h1>
-       Projects
-      </h1>
 
-      <div class="card">
-  <div class="tools">
-    <div class="circle">
-      <span class="red box"></span>
-    </div>
-    <div class="circle">
-      <span class="yellow box"></span>
-    </div>
-    <div class="circle">
-      <span class="green box"></span>
-    </div>
-  </div>
-  <div class="card__content">
-  </div>
-</div>
-    </div>
+    <div>
+    <h1>Projects</h1>
 
-    
+      <div className="cards__container">
+      {Array.from({ length: numberOfCards }, (_, index) => (
+        <div key={index} className={`project_card ${cardStates[index]}`}>
+          <div className="tools">
+            <div className="circle" onClick={() => handleSwitch('red', index)}>
+              <span className="red project--box"></span>
+            </div>
+            <div className="circle" onClick={() => handleSwitch('yellow', index)}>
+              <span className="yellow project--box"></span>
+            </div>
+            <div className="circle" onClick={() => handleSwitch('green', index)}>
+              <span className="green project--box"></span>
+            </div>
+          </div>
+          <div className="card__content">
+            {/* Your card content goes here */}
+          </div>
+        </div>
+      ))}
+      </div>
+    </div>
   );
 }
 
