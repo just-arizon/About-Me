b95303a7118a9f338be052ef21fd7cefd5fef704
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-12-28 01:27:41 +0100

    Project card functionality working perfectly

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import "./Projects.css";
 import TopNav from "../navigation/TopNav";
+import productImage1 from '../images/product10.png'; 
+import productImage2 from '../images/product9.png'; 
 
 function Projects() {
  const numberOfCards = 2;
@@ -12,44 +14,45 @@ function Projects() {
      description: '',
      stacksUsed: '',
    },
-   state: '',
+   state: 'minimized',
  }));
 
  const [cardStates, setCardStates] = useState(initialCardStates);
 
  const cardContent = [
    {
-     image: 'path/to/image1.jpg',
+     image: productImage1,
      title: 'Project 1',
-     description: 'Description for Project 1.',
+     description: 'Description for Project 1 is a project filled with so much inspirations.',
      stacksUsed: 'React, Node.js, MongoDB',
    },
    {
-     image: 'path/to/image1.jpg',
+     image: productImage2,
      title: 'Project 2',
-     description: 'Description for Project 2.',
+     description: 'Description for Project 2 is a project filled with so much inspirations.',
      stacksUsed: 'React, Node.js, MongoDB',
    },
    // Add content for other cards...
  ];
 
  const handleSwitch = (className, index) => {
+  
    const newCardStates = [...cardStates];
 
    switch (className) {
-     case "yellow":
-       newCardStates[index] = 'minimized';
-       break;
-     case "green":
-       newCardStates[index] = '';
-       break;
-     case "red":
-       newCardStates[index] = 'none';
-       break;
-     default:
-       // Handle default case
-   }
-
+    case "yellow":
+      newCardStates[index].state = 'minimized';
+      break;
+    case "green":
+      newCardStates[index].state = '';
+      break;
+    case "red":
+      newCardStates[index].state = 'none';
+      break;
+    default:
+  }
+  
+   
    setCardStates(newCardStates);
  };
 
@@ -72,15 +75,15 @@ function Projects() {
              </div>
            </div>
            <div className="card__content">
-             <img src={cardContent[index].image} alt='Project' />
-             <h2>{cardContent[index].title}</h2>
-             <p>{cardContent[index].description}</p>
-             <p>Stacks Used: {cardContent[index].stacksUsed}</p>
+             <img src={cardContent[index].image} alt='Project' className='card__img'/>
+             <h2 className='card__title'>{cardContent[index].title}</h2>
+             <p className='card__description'>{cardContent[index].description}</p>
+             <p className='card__stacks'>Stacks Used: {cardContent[index].stacksUsed}</p>
            </div>
          </div>
        ))}
      </div>
-   </div>
+ </div>
  );
 }
 
